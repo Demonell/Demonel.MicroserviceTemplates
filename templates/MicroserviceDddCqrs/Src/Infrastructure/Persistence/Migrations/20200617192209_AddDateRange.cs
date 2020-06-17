b6bb25a25f623ba9bd592e73aab7aa548a4ca9e3
A K <gugie194@gmail.com>
2020-06-17 22:24:23 +0300

    Updated version to 1.0.7. Fixed migrations.

---
+++ templates/MicroserviceDddCqrs/Src/Infrastructure/Persistence/Migrations/20200617192209_AddDateRange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddDateRange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastModified",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeliveryDate",
+                table: "Products",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastModified",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+    }
+}
